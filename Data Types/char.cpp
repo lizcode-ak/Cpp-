6df30528927a55f70be94888ce35ab2534705527
Arkendu Chatterjee <heroictatai@gmail.com>
2026-08-06 22:56:42 +0530

    Data Types

---
+++ Data Types/char.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char c = 'M';
+    cout << c;
+    return 0;
+}
---
+++ Data Types/float.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float a = 3.12;
+    float b = 5.11;
+    cout << a+b;
+    return 0;
+}
